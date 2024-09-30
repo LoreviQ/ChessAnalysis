eb85d89356f5359da8fff73d2d631d2eb4189ac1
LoreviQ <lorevi@protonmail.com>
2024-09-30 23:50:08 +0100

    Changed app to run server

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+"""
+Main application to start the server and create the GUI
+"""
+
 import threading
 import tkinter as tk
 
@@ -5,20 +9,46 @@ from database import DBConnection
 from server import initialize_server
 
 
-def start_app():
-    db_connection = DBConnection()
-    my_server = initialize_server(db_connection)
-    server_thread = threading.Thread(
-        target=my_server.run, kwargs={"debug": True, "use_reloader": False}
-    )
-    server_thread.start()
-    root = tk.Tk()
-    root.title("Server Status")
+class App:
+    """
+    Class to create the GUI for the main application
+    """
 
-    data_label = tk.Label(root, text="No data received", font=("Helvetica", 16))
-    data_label.pack(pady=10)
-    root.mainloop()
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
+        self.root = tk.Tk()
+        self.server = None
+        self.server_thread = None
+
+    def start_app(self):
+        """
+        Start the application
+        """
+        # Start the server in a separate thread
+        self.server = initialize_server(self.db_connection)
+        self.server_thread = threading.Thread(target=self._run_server)
+        self.server_thread.start()
+
+        # Create the GUI
+        self._initial_gui()
+        self.root.protocol("WM_DELETE_WINDOW", self._on_close)
+        self.root.mainloop()
+
+    def _run_server(self):
+        self.server.run(debug=True, use_reloader=False, threaded=True)
+
+    def _initial_gui(self):
+        self.root.title("Server Status")
+        data_label = tk.Label(
+            self.root, text="No data received", font=("Helvetica", 16)
+        )
+        data_label.pack(pady=10)
+
+    def _on_close(self):
+        if self.server_thread.is_alive():
+            self.server_thread.join()
+        self.root.destroy()
 
 
 if __name__ == "__main__":
-    start_app()
+    App(DBConnection()).start_app()
