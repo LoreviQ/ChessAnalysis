15341f88de2cc0f9bb4b4f6098101cec2eb486d0
LoreviQ <lorevi@protonmail.com>
2024-09-30 23:11:43 +0100

    Fixed gui to work with new server config

--- app/main.py
+++ app/main.py
@@ -2,39 +2,20 @@ import threading
 import tkinter as tk
 
 from database import DBConnection
-from server import Server
+from server import start_server
 
 
-class App:
-    def __init__(self):
-        self.db_connection = DBConnection()
-        self.server = Server(self.db_connection)
+def start_app():
+    db_connection = DBConnection()
+    server_thread = threading.Thread(target=start_server, args=(db_connection,))
+    server_thread.start()
+    root = tk.Tk()
+    root.title("Server Status")
 
-        self.root = tk.Tk()
-        self.root.title("Server Status")
-
-        self.data_label = tk.Label(
-            self.root, text="No data received", font=("Helvetica", 16)
-        )
-        self.data_label.pack(pady=10)
-
-    def start(self):
-        self.update_gui()
-        self.root.mainloop()
-
-    def update_gui(self):
-        data = self.db_connection.fetch_latest_move()
-        if data:
-            self.data_label.config(text=str(data[-1]))
-        else:
-            self.data_label.config(text="No data received")
-
-        # Schedule the function to run again after 5 seconds
-        self.root.after(5000, self.update_gui)
+    data_label = tk.Label(root, text="No data received", font=("Helvetica", 16))
+    data_label.pack(pady=10)
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    app = App()
-    server_thread = threading.Thread(target=app.server.run())
-    server_thread.start()
-    app.start()
+    start_app()
