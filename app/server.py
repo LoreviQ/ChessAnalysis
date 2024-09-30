4e2c8e7457e89a4cb3fbc647f0e21c077af6138d
LoreviQ <lorevi@protonmail.com>
2024-09-30 20:11:55 +0100

    Refactored server to not use a class

--- app/server.py
+++ app/server.py
@@ -1,43 +1,50 @@
+"""
+Module to define the server class and its routes
+"""
+
 from database import DBConnection
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 
 
-class Server:
-    def __init__(self, db_connection):
-        self.db_connection = db_connection
-        self.server = Flask(__name__)
-        CORS(self.server)
-        self.setup_routes()
+def start_server(db_connection):
+    """
+    Start the Flask server
+    """
+    server = Flask(__name__)
+    CORS(server)
+    setup_routes(server, db_connection)
+    server.run(debug=True, use_reloader=False)
 
-    def setup_routes(self):
-        @self.server.route("/readiness", methods=["GET"])
-        def readiness():
-            """
-            Health check endpoint
-            """
-            return make_response("OK", 200)
 
-        @self.server.route("/update_moves", methods=["POST"])
-        def update_moves():
-            """
-            Update moves endpoint
-            """
-            data = request.get_json()
-            moves = data.get("moves")
-            if not moves:
-                return jsonify({"status": "error", "message": "No moves provided"})
-            game_id = data.get("game_id")
-            print(f"Received moves: {moves}")
-            print(f"Game ID: {game_id}")
-            self.db_connection.insert_move(moves, game_id)
-            return jsonify({"status": "success"})
+def setup_routes(server, db_connection):
+    """
+    Define the server routes
+    """
 
-    def run(self):
-        self.server.run(debug=True, use_reloader=False)
+    @server.route("/readiness", methods=["GET"])
+    def readiness():
+        """
+        Health check endpoint
+        """
+        return make_response("OK", 200)
+
+    @server.route("/update_moves", methods=["POST"])
+    def update_moves():
+        """
+        Update moves endpoint
+        """
+        data = request.get_json()
+        moves = data.get("moves")
+        if not moves:
+            return jsonify({"status": "error", "message": "No moves provided"})
+        game_id = data.get("game_id")
+        print(f"Received moves: {moves}")
+        print(f"Game ID: {game_id}")
+        db_connection.insert_move(moves, game_id)
+        return jsonify({"status": "success"})
 
 
 if __name__ == "__main__":
     db = DBConnection()
-    server = Server(db)
-    server.run()
+    start_server(db)
