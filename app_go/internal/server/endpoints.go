b64a5676395eb6d34059a941ecb6cf40f8428025
LoreviQ <lorevi@protonmail.com>
2024-10-02 18:02:25 +0100

    Added comment to server

--- app_go/internal/server/endpoints.go
+++ app_go/internal/server/endpoints.go
@@ -2,6 +2,9 @@ package server
 
 import "net/http"
 
+// GET /readiness handler
+//
+// This handler is used to check if the server is ready to accept requests.
 func (cfg *serverCfg) getReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
