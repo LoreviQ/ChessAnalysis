a1571b69c7f98a0286650ca542ec341b96d09a78
LoreviQ <lorevi@protonmail.com>
2024-10-02 20:10:40 +0100

    Created endpoint for POST /moves

--- app_go/internal/server/endpoints.go
+++ app_go/internal/server/endpoints.go
@@ -1,10 +1,33 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+)
+
+type postMovesRequest struct {
+	GameID string   `json:"game_id"`
+	Moves  []string `json:"moves"`
+}
 
 // GET /readiness handler
 //
 // This handler is used to check if the server is ready to accept requests.
 func (cfg *serverCfg) getReadiness(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+// POST /moves handler
+func (cfg *serverCfg) postMoves(w http.ResponseWriter, r *http.Request) {
+	var request postMovesRequest
+	err := decodeRequest(w, r, &request)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "failed to decode request body")
+		return
+	}
+	err = cfg.db.InsertMoves(request.Moves, request.GameID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error inserting moves into db")
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 }
---
+++ app_go/internal/server/handlerHelperFunctions.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// DecodeRequest decodes the JSON request body into the provided struct
+func decodeRequest[T any](w http.ResponseWriter, r *http.Request, request T) error {
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return err
+	}
+	return nil
+}
+
+// respondWithJSON sends a JSON response with the provided status code and body
+func respondWithJSON[T any](w http.ResponseWriter, responseCode int, body T) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(&body)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(responseCode)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Printf("Error writing HTTP response: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+}
+
+// respondWithError sends a JSON error response with the provided status code and error message
+func respondWithError(w http.ResponseWriter, responseCode int, errorMsg string) {
+	responseStruct := struct {
+		Error string `json:"error"`
+	}{
+		Error: errorMsg,
+	}
+	data, err := json.Marshal(&responseStruct)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(responseCode)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Printf("Error writing HTTP response: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+}
