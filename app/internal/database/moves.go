2633d22458f1a0e87a55d9d8898d3ec816492d18
LoreviQ <lorevi@protonmail.com>
2024-10-03 16:47:32 +0100

    Extracted fnctions related to the moves table into its own file in database package

---
+++ app/internal/database/moves.go
@@ -0,0 +1,70 @@
+package database
+
+import (
+	"database/sql"
+	"errors"
+	"strings"
+
+	"github.com/LoreviQ/ChessAnalysis/app/internal/game"
+)
+
+// InsertMoves inserts a list of moves into the database
+func (d Database) InsertMoves(moves []string, chessdotcomID string) error {
+	chessdotcomID_NullString := sql.NullString{String: chessdotcomID, Valid: chessdotcomID != ""}
+	standardizedMoves, err := standardizeMoves(moves)
+	if err != nil {
+		return err
+	}
+
+	// Get game id of the latest game with the given chess.com id
+	var gameID int
+	err = d.db.QueryRow(d.queries["GET_LATEST_GAME_ID"], chessdotcomID_NullString).Scan(&gameID)
+	if err == sql.ErrNoRows {
+		// If no game with the given chess.com id exists, create a new game
+		err = d.db.QueryRow(d.queries["INSERT_GAME"], chessdotcomID_NullString).Scan(&gameID)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	// Insert the moves into the database
+	_, err = d.db.Exec(d.queries["INSERT_MOVES"], gameID, standardizedMoves)
+	return err
+}
+
+// GetMoves returns the latest moves of a game with the given chess.com id
+func (d Database) GetMoves(chessdotcomID string) ([]string, error) {
+	var gameID int
+	err := d.db.QueryRow(d.queries["GET_LATEST_GAME_ID"], chessdotcomID).Scan(&gameID)
+	if err != nil {
+		return nil, errors.New("game not found")
+	}
+	var moves string
+	err = d.db.QueryRow(d.queries["GET_LATEST_MOVES"], gameID).Scan(&moves)
+	if err != nil {
+		return nil, errors.New("game has no moves")
+	}
+	return strings.Split(moves, " "), nil
+}
+
+// Converts moves to the format used in the database
+//
+// Expected input: ["1", "e4", "e5", "2", "Nf3", "Nc6", ...]
+// Expected output: "e4 e5 Nf3 Nc6 ..."
+func standardizeMoves(moves []string) (string, error) {
+	// remove turn numbers
+	standardizedMoves := []string{}
+	for i, move := range moves {
+		if i%3 != 0 {
+			standardizedMoves = append(standardizedMoves, move)
+		}
+	}
+	standardizedMoves, err := game.ConvertNotation(standardizedMoves)
+	if err != nil {
+		return "", err
+	}
+	moveString := strings.Join(standardizedMoves, " ")
+	return moveString, nil
+}
---
+++ app/internal/database/moves_test.go
@@ -0,0 +1,45 @@
+package database
+
+import "testing"
+
+// First tests the InsertMoves method by inserting a list of moves into the database
+// and then tests the GetMoves method by retrieving the moves from the database
+func TestInsertGetMoves(t *testing.T) {
+	db, err := NewConnection(true)
+	if err != nil {
+		t.Error(err)
+	}
+	movesToInsert := []string{
+		"1", "e4", "g6", "2", "d4", "Bg7", "3", "e5", "d6",
+		"4", "exd6", "Qxd6", "5", "Nc3", "Bxd4", "6", "Be3", "Bxc3+",
+		"7", "bxc3", "h5", "8", "f4", "Qxd1+", "9", "Rxd1", "Nd7",
+		"10", "Bc4", "e5", "11", "fxe5", "Nxe5", "12", "Bd4", "Nxc4",
+		"13", "Bxh8", "Nb6", "14", "Nf3", "Nd7", "15", "O-O", "Nh6",
+		"16", "Bf6", "Nxf6", "17", "Ng5", "Nd7", "18", "Nxf7", "Nxf7",
+		"19", "Rd3", "Ke7",
+	}
+	expectedMoves := []string{
+		"e2e4", "g7g6", "d2d4", "Bf8g7", "e4e5", "d7d6", "e5xd6", "Qd8xd6",
+		"Nb1c3", "Bg7xd4", "Bc1e3", "Bd4xc3+", "b2xc3", "h7h5", "f2f4",
+		"Qd6xd1+", "Ra1xd1", "Nb8d7", "Bf1c4", "e7e5", "f4xe5", "Nd7xe5",
+		"Be3d4", "Ne5xc4", "Bd4xh8", "Nc4b6", "Ng1f3", "Nb6d7", "O-O", "Ng8h6",
+		"Bh8f6", "Nd7xf6", "Nf3g5", "Nf6d7", "Ng5xf7", "Nh6xf7", "Rd1d3", "Ke8e7",
+	}
+	err = db.InsertMoves(movesToInsert, "123456")
+	if err != nil {
+		t.Error(err)
+	}
+	moves, err := db.GetMoves("123456")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(moves) != len(expectedMoves) {
+		t.Errorf("Expected %d moves, got %d", len(expectedMoves), len(moves))
+	}
+	for i := range moves {
+		if moves[i] != expectedMoves[i] {
+			t.Errorf("Expected move %s, got %s", expectedMoves[i], moves[i])
+		}
+	}
+	db.Close()
+}
